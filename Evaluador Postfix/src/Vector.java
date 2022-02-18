230e462613a1f31c454f1e3f1d0c12b81a8faf1b
Sly <64713554+SlyRowe@users.noreply.github.com>
2022-02-18 13:22:23 -0600

    Update Vector.java

--- Evaluador Postfix/src/Vector.java
+++ Evaluador Postfix/src/Vector.java
@@ -29,8 +29,85 @@ public class Vector<T> implements IStack<T>, IPosfixCalc {
 			token = scan.next();
 			
 			//si el token es un operador, se evalua la expresion
+			if (esOperador(token)) 
+			{
+				//se jalan los numeros de la pila
+				op2 = (stack.pull()).intValue();
+				op1 = (stack.pull()).intValue();
+				
+				resultado = operaciones(token.charAt(0), op1, op2);
+				
+			} else 
+			{
+				//si el caracter es un numero lo ingresa a la pila
+				try 
+				{
+					stack.push(Integer.parseInt(token));
+				} catch (NumberFormatException e)
+				{
+					System.out.println("Solo se aceptan operadores aritméticos o enteros");
+					break;
+				}
+				
+			}
 			
+		}
+		
+		return resultado;
+		
+	}
+	
+	/**
+	 * este metodo privado evalua si el siguiente caracter es un operador, ej.: +
+	 * @param el siguiente caracter
+	 * @return true o false
+	 */
+	private boolean esOperador(String token) {
+		
+		return (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/"));
+		
+	}
+
+	/**
+	 * este metodo realiza las operaciones aritméticas
+	 * @param detecta que tipo de expresión es a través del operador
+	 * @param el primer numero ingresado en la pila
+	 * @param el segundo numero ingresado en la pila
+	 * @return el resultado de la expresion
+	 */
+	private int operaciones(char expresion, int op1, int op2) {
+		
+		int resultado = 0;
+		
+		//segun el operador ingresado, se realiza esa operación
+		switch(expresion) 
+		{
+		
+		case '+':
+			resultado = op1 + op2;
+		break;
+		
+		case '-':
+			resultado = op1 - op2;
+		break;
+		
+		case '*':
+			resultado = op1 * op2;
+		break;
+		
+		case '/':
 			
+			try 
+			{
+				resultado = op1 / op2;
+			} 
+			catch (ArithmeticException e) 
+			{
+				System.out.println("No se puede dividir dentro de 0");
+			}
+			
+		break;
+		
 		}
 		
 		return resultado;
